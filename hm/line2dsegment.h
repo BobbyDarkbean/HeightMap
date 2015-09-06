1da954fa9d7ebe74cfc913ed46cb0cba92a055ae
BobbyDarkbean <monsieurbobah@yandex.ru>
2015-09-06 04:21:00 +0300

    PeakInfo, Line2dSegment added

---
+++ hm/line2dsegment.h
@@ -0,0 +1,21 @@
+#ifndef _Line2dSegment_h_
+#define _Line2dSegment_h_
+
+
+#include "hm_shared.h"
+
+
+namespace HeightMap {
+
+
+struct HM_SHARED Line2dSegment
+{
+    double x1, y1, x2, y2;
+    int level;
+};
+
+
+}
+
+
+#endif // _Line2dSegment_h_
---
+++ hm/peakinfo.h
@@ -0,0 +1,21 @@
+#ifndef _PeakInfo_h_
+#define _PeakInfo_h_
+
+
+#include "hm_shared.h"
+
+
+namespace HeightMap {
+
+
+struct HM_SHARED PeakInfo
+{
+    int x, y;
+    double height;
+};
+
+
+}
+
+
+#endif // _PeakInfo_h_
