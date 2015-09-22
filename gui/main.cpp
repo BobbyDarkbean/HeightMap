e23a4e5a33fb86c44b6e83aa1a487ee09518ce60
Bobby Bob <monsieurbobah@yandex.ru>
2015-09-22 04:43:40 +0300

    application name/window title

--- gui/main.cpp
+++ gui/main.cpp
@@ -4,8 +4,10 @@
 int main(int argc, char **argv)
 {
     HeightMap::HeightMapApplication app(argc, argv);
+    app.setApplicationName("HeightMap");
 
     HeightMap::HeightMapWindow w;
+    w.setWindowTitle(app.applicationName());
     w.showMaximized();
 
     return app.exec();
