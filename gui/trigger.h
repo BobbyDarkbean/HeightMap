c479be8dc49b0f9fab1956a3ffda4c0855d3f032
Bobby Bob <monsieurbobah@yandex.ru>
2015-10-02 22:50:23 +0300

    Trigger class added

---
+++ gui/trigger.h
@@ -0,0 +1,33 @@
+#ifndef _Trigger_h_
+#define _Trigger_h_
+
+
+#include <QObject>
+#include "hm_shared.h"
+
+
+namespace HeightMap {
+
+
+class Trigger : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit Trigger(QObject *parent = 0);
+    virtual void activate();
+    ~Trigger();
+
+signals:
+    void activated();
+
+private:
+    DISABLE_COPY(Trigger)
+    DISABLE_MOVE(Trigger)
+};
+
+
+} // namespace HeightMap
+
+
+#endif // _Trigger_h_
