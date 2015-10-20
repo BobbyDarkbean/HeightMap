ee752b53ae329a8351395ba21cf0316a638c264a
Bobby Bob <monsieurbobah@yandex.ru>
2015-10-20 18:27:26 +0300

    HeightMapBinaryIOHandler implemented

--- io/heightmapbinaryiohandler.cpp
+++ io/heightmapbinaryiohandler.cpp
@@ -12,6 +12,19 @@ HeightMapBinaryIOHandler::HeightMapBinaryIOHandler()
     : HeightMapIOHandler() { }
 
 
+Preferences HeightMapBinaryIOHandler::preferences() const
+{ return m_prefs; }
+
+void HeightMapBinaryIOHandler::setPreferences(const Preferences &prefs)
+{ m_prefs = prefs; }
+
+ExtrapolationData HeightMapBinaryIOHandler::xData() const
+{ return m_xdata; }
+
+void HeightMapBinaryIOHandler::setXData(const ExtrapolationData &data)
+{ m_xdata = data; }
+
+
 void HeightMapBinaryIOHandler::write(const Terrain *t, const QString &filename)
 {
     QFile f(filename);
@@ -22,6 +35,12 @@ void HeightMapBinaryIOHandler::write(const Terrain *t, const QString &filename)
     QDataStream stream(&f);
     stream.setVersion(QDataStream::Qt_5_0);
 
+    stream << m_prefs << m_xdata;
+    for (std::vector<PeakInfo>::const_iterator i = t->peaks().begin(); i != t->peaks().end(); ++i) {
+        stream << static_cast<quint32>(i->x)
+               << static_cast<quint32>(i->y)
+               << i->height;    // TODO: cast to independent format
+    }
 }
 
 void HeightMapBinaryIOHandler::read(Terrain *t, const QString &filename)
@@ -34,6 +53,18 @@ void HeightMapBinaryIOHandler::read(Terrain *t, const QString &filename)
     QDataStream stream(&f);
     stream.setVersion(QDataStream::Qt_5_0);
 
+    stream >> m_prefs >> m_xdata;
+    std::vector<PeakInfo> peaks(m_prefs.peakCount());
+    for (std::vector<PeakInfo>::iterator i = peaks.begin(); i != peaks.end(); ++i) {
+        quint32 x, y;
+        double height;
+
+        stream >> x >> y >> height;
+        i->x = static_cast<int>(x);
+        i->y = static_cast<int>(y);
+        i->height = height;     // TODO: cast from independent format
+    }
+    t->setPeaks(peaks);
 }
 
 
--- io/heightmapbinaryiohandler.h
+++ io/heightmapbinaryiohandler.h
@@ -3,6 +3,8 @@
 
 
 #include "heightmapiohandler.h"
+#include "preferences.h"
+#include "extrapolationdata.h"
 
 
 namespace HeightMap {
@@ -13,10 +15,20 @@ class HMIO_SHARED HeightMapBinaryIOHandler : public HeightMapIOHandler
 public:
     HeightMapBinaryIOHandler();
 
+    Preferences preferences() const;
+    void setPreferences(const Preferences &);
+
+    ExtrapolationData xData() const;
+    void setXData(const ExtrapolationData &);
+
     void write(const Terrain *t, const QString &filename);
     void read(Terrain *t, const QString &filename);
 
     ~HeightMapBinaryIOHandler();
+
+private:
+    Preferences m_prefs;
+    ExtrapolationData m_xdata;
 };
 
 
